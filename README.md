60f90d5b00e516fa49a76ad845d31d24efa6caf9
Daniel McAssey <daniel.mcassey@gmail.com>
2025-01-24 01:29:51 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# baiters-server-plugin-example
-An example plugin for WebFishing Baiters Server
+# Baiters Plugin Example
+[![MyExamplePluginForBaiters](https://github.com/DanielMcAssey/baiters-server/actions/workflows/release.yml/badge.svg)](https://github.com/DanielMcAssey/baiters-server/actions/workflows/release.yml)
+
+An example plugin for WebFishing Baiters Server, it shows you how to create it, its ready to go for publishing changes automatically to GitHub with GitHub Actions, and supports main, beta and alpha branches and releases
+
+
+## Customisation
+> [!NOTE]
+> Below are the list of files you will likely want to change to make it your own
+
+1. **src/MyExamplePlugin.cs** rename this file to your plugin name
+2. **src/MyExamplePluginForBaiters.csproj** rename this file to your plugin name
+3. **MyExamplePluginForBaiters.sln** rename this file to your plugin name, update the `csproj` file path to the one you changed in step 2
+4. **.github/workflows/build.yaml** change the `PLUGIN_PROJECT` environment variable to the one you changed in step 2
+5. **.github/workflows/release.yaml** change the `PLUGIN_PROJECT` environment variable to the one you changed in step 2, change the `PLUGIN_NAME` to your plugin name
+6. **README.md** update this file and tell users how to they can use your plugin, I have included a boilerplate `Setup` section, if you want to use it, make sure you update the references, if you want to keep the status badge at the top, update the reference to your repository
+
+Then you are ready to go! Use your favourite IDE, I recommend VS Code 
+
+
+## Install Plugin
+> [!IMPORTANT]
+> If you want to use this plugin, you will need to enable plugins on Baiters Server if you havent already
+
+1. Download the latest plugin `MyExamplePluginForBaiters-vX.X.X.zip` from the releases page
+2. Unzip the plugin to a folder
+3. Copy the contents of that folder to the Baiters Server `plugins` folder
+4. Restart the Server, if the plugin was installed successfully, the server will list this plugin
---
+++ src/MyExamplePlugin.cs
@@ -0,0 +1,117 @@
+﻿using GLOKON.Baiters.Core;
+using GLOKON.Baiters.Core.Enums.Networking;
+using GLOKON.Baiters.Core.Models.Networking;
+using GLOKON.Baiters.Core.Plugins;
+using System.Numerics;
+using System.Reflection;
+
+namespace MyExamplePluginForBaiters
+{
+    public sealed class MyExamplePlugin(GameManager gm) : BaitersPlugin(
+        gm,
+        "My Example Plugin",
+        "A plugin to show how to make a plugin!",
+        "Daniel McAssey <dan@glokon.me>",
+        Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0.0")
+    {
+        public override void OnInit()
+        {
+            // Use this to setup your plugin, called when the server is starting up, like hooking up to events
+            base.OnInit();
+
+            // Lets listen to some events
+            GM.Server.OnPlayerJoined += Server_OnPlayerJoined;
+            GM.Server.OnPlayerLeft += Server_OnPlayerLeft;
+            GM.Server.OnChatMessage += Server_OnChatMessage;
+            GM.Server.OnLobbyChatMessage += Server_OnLobbyChatMessage;
+            GM.Server.OnTick += Server_OnTick;
+            GM.Server.OnPacket += Server_OnPacket;
+
+            // Lets listen to a custom chat command, exclude the prefix, as its set by the server admin (Defaults to "!")
+            GM.Chat.ListenFor("my-cool-command", "My helpful text to explain my command", (fromSteamId, commandParams) =>
+            {
+                GM.Server.SendSystemMessage("This is a message I am sending", steamId: fromSteamId);
+
+                GM.Server.SendPacket(new("my_cool_packet_two")
+                {
+                    ["pos"] = Vector3.Zero,
+                    ["rot"] = Vector3.Zero,
+                    ["potato"] = false,
+                    ["plant_scale"] = 4.2f,
+                }, DataChannel.Guitar);
+            });
+        }
+
+        public override void OnDestroy()
+        {
+            // Use this to clean-up your plugin, called when the server is shutting down, like cleaning up events
+            base.OnDestroy();
+
+            // Lets cleanup the events
+            GM.Server.OnPlayerJoined -= Server_OnPlayerJoined;
+            GM.Server.OnPlayerLeft -= Server_OnPlayerLeft;
+            GM.Server.OnChatMessage -= Server_OnChatMessage;
+            GM.Server.OnLobbyChatMessage -= Server_OnLobbyChatMessage;
+            GM.Server.OnTick -= Server_OnTick;
+            GM.Server.OnPacket -= Server_OnPacket;
+
+            // Stop listening to Chat Commands
+            GM.Chat.StopListening("my-cool-command");
+        }
+
+        public override bool CanPlayerJoin(ulong steamId)
+        {
+            // Use this to check if a player can join, if you return false, the player is blocked from joining
+            return base.CanPlayerJoin(steamId);
+        }
+
+        private void Server_OnPlayerJoined(ulong steamId)
+        {
+            // Called when a player has joined fully
+        }
+
+        private void Server_OnPlayerLeft(ulong steamId)
+        {
+            // Called when the player has left
+        }
+
+        private void Server_OnChatMessage(ulong fromSteamId, string message)
+        {
+            // Called when a new chat message was received
+        }
+
+        private void Server_OnLobbyChatMessage(ulong fromSteamId, string message)
+        {
+            // Called when theres a new message on the Lobby chat
+        }
+
+        private void Server_OnTick()
+        {
+            // Try not to do any intense work here, this is called on every server tick (By default 24 ticks per second)
+        }
+
+        private void Server_OnPacket(ulong fromSteamId, Packet packet)
+        {
+            // Called when the server receives a packet, this could happen very frequently
+
+            // An example below of using a custom packet (For example from a mod)
+            if (packet.Type == "my_cool_packet_type")
+            {
+                GM.Actioner.MovePlayer(fromSteamId, (Vector3)packet["pos"], (Vector3)packet["rot"]);
+
+                GM.Spawner.SpawnFish();
+                GM.Spawner.SpawnVoidPortal();
+
+                foreach (var ownedActor in GM.Server.OwnedActors)
+                {
+                    // Lets be "wild" any ownerID above 1 million gets removed, becase we dont like big numbers ...right?
+                    if (ownedActor.Value.OwnerId > 1_000_000)
+                    {
+                        // The key in OwnedActors and Actors is the ID
+                        GM.Server.RemoveActor(ownedActor.Key);
+                    }
+                }
+            }
+        }
+    }
+}
